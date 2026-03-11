4754c8233571540d860f836c477335916d7a0454
BRunknown <asafe.ferr@gmail.com>
2026-03-11 20:06:12 -0300

    feat: add middleware and lifespan feature

--- parte_2/microservice_nre/main.py
+++ parte_2/microservice_nre/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 
+from microservice_nre.lifespan import lifespan
+from microservice_nre.middleware import request_middleware
 from microservice_nre.routers import health, model, predict
 
-app = FastAPI(title='NRE Service')
+app = FastAPI(title='NRE Service', lifespan=lifespan)
 
+# intercepta requisições para adicionar informações rastreio
+app.middleware('http')(request_middleware)
+
+# roteadores para endpoints da aplicação
 app.include_router(model.router)
 app.include_router(health.router)
 app.include_router(predict.router)
 
 
 @app.get('/')
-async def root() -> None:
+async def root() -> dict[str, str]:
     return {'message': 'Hello World'}
